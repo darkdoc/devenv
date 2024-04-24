6360223f26800c8a44504e99796aa993d755e84d
Akos Eros <erosakos88@gmail.com>
2024-04-24 22:14:13 +0200

    feat: Add initial verison of setup script

---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# Set color variables for some nice-er output
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
+REPO_URL=https://github.com/darkdoc/devenv.git
+REPO_PATH="$HOME/.devenv"
+
+function info {
+  echo -e "Info: ${1}"
+}
+
+function success {
+  echo -e "${GREEN}Success: ${RESET}${1}"
+}
+
+function warning {
+  echo -e "${YELLOW}Warning: ${RESET}${1}"
+}
+
+function error {
+  echo -e "${RED}Error: ${RESET}${1}"
+  exit 1
+}
+
+if [[ $EUID -eq 0 ]]; then
+   error "This script should not be run as root, but it will need sudo for installing" 
+   exit 1
+fi
+
+function install_deb {
+    info "Installing python3,pip,git..."
+    sudo apt install python3 python3-pip git -y
+    
+    info "Cloning devenv repo ... (this might override any prev devenv cloned)"
+    if [ -d $REPO_PATH ]; then 
+        warning "Directory $REPO_PATH exists. Fetching there anyways..."
+        cd $REPO_PATH
+        git init --initial-branch=main
+        git remote add origin $REPO_URL
+        git pull -f   || error "Git pull failed"
+    else
+        git clone "$REPO_URL" "$REPO_PATH" || error "Git clone failed"
+        cd $REPO_PATH
+    fi
+
+    info "Installing venv for user..."
+
+    python3 -m pip install --user virtualenv
+    virtualenv venv
+    . venv/bin/activate
+
+    info "Installing ansible"
+    pip install ansible
+    info "$dist_type"
+    ansible-playbook playbook.yml
+}
+
+#function install_rpm {
+#    info "Installing pip..."
+#    python3 -m ensurepip --upgrade
+#}
+
+case "${1}" in
+  (--apt|--deb)
+    command -v apt >/dev/null 2>&1 || error "apt not found"
+    export dist_type="Debian"
+    echo "$dist_type"
+    install_deb
+  ;;
+  (--dnf|--rpm|--yum)
+    command -v yum >/dev/null 2>&1 || error "yum not found"
+    dist_type=RedHat
+    #install_rpm #TODO implement
+  ;;
+  (''|*)
+      info "No option given, defaulting to ubuntu/deb"
+      command -v apt >/dev/null 2>&1 || error "apt not found"
+      install_deb
+  ;;
+esac
